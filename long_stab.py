332c900d56f08bfce2d90e8837cf612c3d2260fe
Kelden <kjbenora@ucdavis.edu>
2022-02-10 20:37:32 -0800

    ran xfoil and xfoil is conditional

--- long_stab.py
+++ long_stab.py
@@ -29,7 +29,7 @@ mu = 1.802e-5 # kg/m*s
 
 W = 55 # lbs
 
-airfoil_c = 'naca0012'
+airfoil_c = 'naca2415'
 airfoil_f = 'n24'
 airfoil_w = 'naca2415'
 
@@ -61,15 +61,15 @@ def can_alpha_req(xcg, xc, xf, xw, bc, bw, bf, cc, cf, cw, airfoil_c, airfoil_f,
     Re_f = rho * u_inf * cf / mu
     Re_w = rho * u_inf * cw / mu
     
-    airfoil_c_path = os.path.join('data', airfoil_c)
+    airfoil_c_path = os.path.join('data', f'{airfoil_c}.dat')
     with open(airfoil_c_path, 'r') as infile:
         x_c, y_c = np.loadtxt(infile, unpack=True, skiprows=1)
         
-    airfoil_f_path = os.path.join('data', airfoil_f)
+    airfoil_f_path = os.path.join('data', f'{airfoil_f}.dat')
     with open(airfoil_f_path, 'r') as infile:
         x_f, y_f = np.loadtxt(infile, unpack=True, skiprows=1)
     
-    airfoil_w_path = os.path.join('data', airfoil_w)
+    airfoil_w_path = os.path.join('data', f'{airfoil_w}.dat')
     with open(airfoil_w_path, 'r') as infile:
         x_w, y_w = np.loadtxt(infile, unpack=True, skiprows=1)    
     
@@ -80,18 +80,27 @@ def can_alpha_req(xcg, xc, xf, xw, bc, bw, bf, cc, cf, cw, airfoil_c, airfoil_f,
     alphas = np.linspace(start=start, stop=stop, num=num)
     naca = False
     
-    foil = f'data/{airfoil_c}.dat'
-    p.GetPolar(foil, naca, alphas, Re_c, pane=True)
-    foil = f'data/{airfoil_f}.dat'
-    p.GetPolar(foil, naca, alphas, Re_f, pane=True)
-    foil = f'data/{airfoil_w}.dat'
-    p.GetPolar(foil, naca, alphas, Re_w, pane=True)
-    
-    f_c = f'data/{airfoil_c}/{airfoil_c}_polar_Re{Re_c:.2e}a{start:.2f}-{stop:.2f}.dat'
+    f_c = f'data/{airfoil_c}/{airfoil_c}_polar_Re{Re_c:.2e}a{start:.1f}-{stop:.1f}.dat'
+    f_f = f'data/{airfoil_f}/{airfoil_f}_polar_Re{Re_f:.2e}a{start:.1f}-{stop:.1f}.dat'
+    f_w = f'data/{airfoil_w}/{airfoil_w}_polar_Re{Re_w:.2e}a{start:.1f}-{stop:.1f}.dat'
+
+    if not os.path.exists(f_c):
+        foil = f'data/{airfoil_c}.dat'
+        p.GetPolar(foil, naca, alphas, Re_c, pane=True)
+        print('did xfoil')
+
+    if not os.path.exists(f_f):
+        foil = f'data/{airfoil_f}.dat'
+        p.GetPolar(foil, naca, alphas, Re_f, pane=True)
+        print('did xfoil')
+
+    if not os.path.exists(f_w):
+        foil = f'data/{airfoil_w}.dat'
+        p.GetPolar(foil, naca, alphas, Re_w, pane=True)
+        print('did xfoil')
+  
     canard_data = np.loadtxt(f_c, unpack=True, skiprows=12)
-    f_f = f'data/{airfoil_f}/{airfoil_f}_polar_Re{Re_f:.2e}a{start:.2f}-{stop:.2f}.dat'
     fuselage_data = np.loadtxt(f_f, unpack=True, skiprows=12)
-    f_w = f'data/{airfoil_w}/{airfoil_w}_polar_Re{Re_w:.2e}a{start:.2f}-{stop:.2f}.dat'
     wing_data = np.loadtxt(f_w, unpack=True, skiprows=12)
     
     
@@ -99,13 +108,70 @@ def can_alpha_req(xcg, xc, xf, xw, bc, bw, bf, cc, cf, cw, airfoil_c, airfoil_f,
     
 
     
-def total_lift(bc, bw, bf, u_inf, alf_main, alf_c):
-    # calculate total lift of aircraft
-    Sc = c.in2m(cc) * c.in2m(bc)
-    Sf = c.in2m(cf) * c.in2m(bf)
-    Sw = c.in2m(cw) * c.in2m(bw)
+def total_lift(alpha_c, alpha_wf, bc, bw, bf, cc, cf, cw, airfoil_c, airfoil_f, airfoil_w, rho, mu, W, u_inf):
+    # calculate total lift at given angles of attack
+    # conversions
+    cc = c.in2m(cc)
+    cf = c.in2m(cf)
+    cw = c.in2m(cw)
     
+    bc = c.in2m(bc)
+    bf = c.in2m(bf)
+    bw = c.in2m(bw)
     
+    u_inf = c.ftpers2mpers(c.mph2ftpers(u_inf))
+    
+    # surface areas
+    Sc = cc * bc
+    Sf = cf * bf
+    Sw = cw * bw
+    
+    # reynolds of each 'wing'
+    Re_c = rho * u_inf * cc / mu
+    Re_f = rho * u_inf * cf / mu
+    Re_w = rho * u_inf * cw / mu
+    
+    airfoil_c_path = os.path.join('data', f'{airfoil_c}.dat')
+    with open(airfoil_c_path, 'r') as infile:
+        x_c, y_c = np.loadtxt(infile, unpack=True, skiprows=1)
+        
+    airfoil_f_path = os.path.join('data', f'{airfoil_f}.dat')
+    with open(airfoil_f_path, 'r') as infile:
+        x_f, y_f = np.loadtxt(infile, unpack=True, skiprows=1)
+    
+    airfoil_w_path = os.path.join('data', f'{airfoil_w}.dat')
+    with open(airfoil_w_path, 'r') as infile:
+        x_w, y_w = np.loadtxt(infile, unpack=True, skiprows=1)    
+    
+    #running xfoil
+    start = 0
+    stop = 20
+    num = 50
+    alphas = np.linspace(start=start, stop=stop, num=num)
+    naca = False
+    
+    f_c = f'data/{airfoil_c}/{airfoil_c}_polar_Re{Re_c:.2e}a{start:.1f}-{stop:.1f}.dat'
+    f_f = f'data/{airfoil_f}/{airfoil_f}_polar_Re{Re_f:.2e}a{start:.1f}-{stop:.1f}.dat'
+    f_w = f'data/{airfoil_w}/{airfoil_w}_polar_Re{Re_w:.2e}a{start:.1f}-{stop:.1f}.dat'
+
+    if not os.path.exists(f_c):
+        foil = f'data/{airfoil_c}.dat'
+        p.GetPolar(foil, naca, alphas, Re_c, pane=True)
+        print(f'did xfoil on {airfoil_c}')
+
+    if not os.path.exists(f_f):
+        foil = f'data/{airfoil_f}.dat'
+        p.GetPolar(foil, naca, alphas, Re_f, pane=True)
+        print(f'did xfoil on {airfoil_f}')
+
+    if not os.path.exists(f_w):
+        foil = f'data/{airfoil_w}.dat'
+        p.GetPolar(foil, naca, alphas, Re_w, pane=True)
+        print(f'did xfoil on {airfoil_w}')
+  
+    canard_data = np.loadtxt(f_c, unpack=True, skiprows=12)
+    fuselage_data = np.loadtxt(f_f, unpack=True, skiprows=12)
+    wing_data = np.loadtxt(f_w, unpack=True, skiprows=12)
     
     
     
